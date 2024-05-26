af3b170f2fbae3633fe0f119c3be1f90d8740c2e
xibalbam <maelporret@outlook.fr>
2024-05-26 22:22:25 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    application
 }
 
 group = "fr.xibalba"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.XibalbaM:PronoteKt:0e10371989")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
@@ -18,4 +23,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = "MainKt"
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import fr.xibalba.pronoteKt.Ent
+import fr.xibalba.pronoteKt.Period
+import fr.xibalba.pronoteKt.PronoteKt
+import fr.xibalba.pronoteKt.SessionType
+import fr.xibalba.pronoteKt.getNotes
+import fr.xibalba.pronoteKt.gson
+import java.io.File
+import java.net.URI
+import java.net.URLEncoder
+
+val gson = Gson()
+
+suspend fun main() {
+    val pronote = PronoteKt("https://0740006e.index-education.net/pronote", SessionType.STUDENT, Ent.AUVERGNE_RHONE_ALPES)
+    pronote.login(System.getenv("PRONOTE_USER"), System.getenv("PRONOTE_PASS"))
+    val notes = pronote.getNotes(Period.THIRD_TRIMESTER).notes
+    val notesWithIds = notes.associateWith { it.dateString + it.subject + it.mark }
+    val oldNotes: List<String> = URI("https://raw.githubusercontent.com/XibalbaM/NewNoteNotificator/master/notes.json?token=GHSAT0AAAAAACRTFKCWM4NUXOK5BUIJDG2SZSTTTEQ")
+        .toURL().readText()
+        .let {
+            class Token : TypeToken<List<String>>()
+            gson.fromJson(it, Token().type)
+        }
+    val newNotes = notesWithIds.filter { it.value !in oldNotes }
+    newNotes.forEach {
+        val notificationText = "Nouvelle note en ${it.key.subject} : ${it.key.mark}" +
+                if (it.key.title.isNotEmpty()) " (${it.key.title})" else ""
+        URI("https://trigger.macrodroid.com/77230727-b9a3-4a6f-bf5a-84ddffeb1830/new_grade?text="
+                + URLEncoder.encode(notificationText, "UTF-8")).toURL().readText()
+    }
+    val file = File("notes.json")
+    file.writeText(gson.toJson(notesWithIds.values.toList()))
+}
