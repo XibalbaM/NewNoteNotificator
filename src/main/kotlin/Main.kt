6f194425f5c57cffe449d3ee22b5902795c01ea8
Xibalba <85184262+XibalbaM@users.noreply.github.com>
2024-06-02 12:36:12 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,21 +14,19 @@ suspend fun main() {
     val pronote = PronoteKt("https://0740006e.index-education.net/pronote", SessionType.STUDENT, Ent.AUVERGNE_RHONE_ALPES)
     pronote.login(System.getenv("PRONOTE_USER"), System.getenv("PRONOTE_PASS"))
     val notes = pronote.getNotes(Period.THIRD_TRIMESTER).notes
-    val notesWithIds = notes.associateWith { it.hashCode().toHexString() }
+    val notesWithIds = notes.associateWith { (it.dateString+it.subject+it.mark+it.title).hashCode().toHexString() }
     val oldNotes: List<String> = URI("https://raw.githubusercontent.com/XibalbaM/NewNoteNotificator/master/notes.json")
         .toURL().readText()
         .let {
             class Token : TypeToken<List<String>>()
             gson.fromJson(it, Token().type)
         }
-    println("Old notes: $oldNotes")
-    println("New notes: $notesWithIds")
     val newNotes = notesWithIds.filter { it.value !in oldNotes }
     newNotes.forEach {
         val notificationText = "Nouvelle note en ${it.key.subject} : ${it.key.mark}" +
                 if (it.key.title.isNotEmpty()) " (${it.key.title})" else ""
         URI("https://trigger.macrodroid.com/${System.getenv("NOTIF_CODE")}/new_grade?text="
-                + URLEncoder.encode(notificationText, "UTF-8")).toURL().also {println(it)}.readText()
+                + URLEncoder.encode(notificationText, "UTF-8")).toURL().readText()
     }
     val file = File("notes.json")
     file.writeText(gson.toJson(notesWithIds.values.toList()))
